3f7ef1c992c8f43a8338746b870df49c94fcbf24
Tom Counsell <tamc@greenonblack.com>
2015-05-14 10:05:40 +0100

    Remove debug print statement

--- lib/extract_overall_cost_and_emissions.rb
+++ lib/extract_overall_cost_and_emissions.rb
@@ -26,7 +26,6 @@ class ExtractOverallCostAndEmissions
   end
 
   def extract_overall_cost_and_emissions_from(name, gdx)
-    p name, gdx.scenarios
     results = { name: name }
 	  cost = gdx.objz
     results[:cost] = cost / 1.0e6 # Convert from £M to £trn
